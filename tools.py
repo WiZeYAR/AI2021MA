2bb412e52f9176237a7d78ace451d18968ad60e9
UmbertoJr <u.mele.coding@gmail.com>
2020-09-23 19:38:19 +0200

    init AI 2020

--- tools.py
+++ tools.py
@@ -1,17 +1,24 @@
 
-class import_methods:
 
-    def __init__(self):
+class import_methods:
+    """
+    the class is responsable to import libraries and methods in the right directory,
+    since that there is a conflict between colaboratory and local runs
+    """
+    def __init__(self, library):
         import os
         curr_dir = os.getcwd()
 
         if 'AI' in curr_dir:
-            if 'solvers' in curr_dir:
+            if 'solvers' in library:
                 from solvers.utils import *
                 from solvers.constructive_algorithms import *
                 from solvers.local_search import *
+
+            if 'IO' in library:
+                from IO_manager
         else:
-            if 'solvers' in curr_dir:
+            if 'solvers' in library:
                 from AI2020.solvers.utils import *
                 from AI2020.solvers.constructive_algorithms import *
                 from AI2020.solvers.local_search import *
