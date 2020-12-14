d55b686102bd93c4811e0e0c79b69c9fa6b6b6f5
UmbertoJr <u.mele.coding@gmail.com>
2020-12-14 15:20:28 +0100

    lab2 AI 2020

--- solvers/two_opt_with_candidate.py
+++ solvers/two_opt_with_candidate.py
@@ -8,8 +8,14 @@ class TwoOpt_CL:
         seq_length = len(solution)
         tsp_sequence = np.array(solution)
         uncrosses = 0
+        ite = 0
         for i in range(1, seq_length-2):
             for j_ in cand_list[i]:
+                ite += 1
+                if ite > 11:
+                    break
+
+
                 j = np.argwhere(tsp_sequence == j_)[0][0]
 
                 if j > i:
@@ -96,14 +102,10 @@ class TwoOpt_CL:
         new_tsp_sequence = np.copy(np.array(solution))
         N = len(solution)
         uncross = 0
-        ite = 0
         while True:
             new_tsp_sequence = np.roll(new_tsp_sequence, np.random.randint(1, N))
             new_tsp_sequence, new_reward, uncr_ = TwoOpt_CL.step2opt(new_tsp_sequence, matrix_dist, actual_len, CL, N)
             uncross += uncr_
-            ite += 1
-            if ite > 11:
-                break
             if new_reward < actual_len:
                 print(actual_len, new_reward)
                 actual_len = new_reward
