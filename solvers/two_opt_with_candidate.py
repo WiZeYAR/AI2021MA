9895f0817acbd7e7366532debb8feca7edb6b8b6
UmbertoJr <u.mele.coding@gmail.com>
2020-12-14 13:02:46 +0100

    lab2 AI 2020

--- solvers/two_opt_with_candidate.py
+++ solvers/two_opt_with_candidate.py
@@ -9,7 +9,8 @@ class TwoOpt_CL:
         tsp_sequence = np.array(solution)
         uncrosses = 0
         for i in range(1, seq_length):
-            for j in cand_list[i]:
+            for j_ in cand_list[i]:
+                j = np.argwhere(tsp_sequence == j_)
                 if j==N:
                     j = 0
                 new_distance = distance + TwoOpt_CL.gain(i - 1, j, tsp_sequence, matrix_dist)
@@ -24,16 +25,16 @@ class TwoOpt_CL:
     @staticmethod
     def swap2opt(tsp_sequence, i, j):
         new_tsp_sequence = np.copy(tsp_sequence)
-        final_index = j + 1
+        final_index = j+1
         new_tsp_sequence[i:final_index] = np.flip(tsp_sequence[i:final_index], axis=0)
         return new_tsp_sequence
 
     @staticmethod
     def gain(i, j, tsp_sequence, matrix_dist):
-        old_link_len = (matrix_dist[tsp_sequence[i], tsp_sequence[i - 1]] + matrix_dist[
-            tsp_sequence[j], tsp_sequence[j + 1]])
-        changed_links_len = (matrix_dist[tsp_sequence[j], tsp_sequence[i - 1]] + matrix_dist[
-            tsp_sequence[i], tsp_sequence[j + 1]])
+        old_link_len = (matrix_dist[tsp_sequence[i], tsp_sequence[i - 1]] +
+                        matrix_dist[tsp_sequence[j], tsp_sequence[j + 1]])
+        changed_links_len = (matrix_dist[tsp_sequence[j], tsp_sequence[i - 1]] +
+                             matrix_dist[tsp_sequence[i], tsp_sequence[j + 1]])
         return - old_link_len + changed_links_len
 
     @staticmethod
