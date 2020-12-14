fa24ce472f27ecc5f5ccc63d262506ebeb15747b
UmbertoJr <u.mele.coding@gmail.com>
2020-12-14 18:21:18 +0100

    lab2 AI 2020

--- solvers/setup.py
+++ solvers/setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 from Cython.Build import cythonize
 
 setup(
-    ext_modules=cythonize("AI2020/solvers/two_opt_with_candidate.pyx"),
+    ext_modules=cythonize("solvers/two_opt_with_candidate.pyx"),
 )
