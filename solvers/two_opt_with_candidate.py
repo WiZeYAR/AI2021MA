37bb09edcecbdca72d34e1fdf14ccd22e2ef2141
UmbertoJr <u.mele.coding@gmail.com>
2020-12-14 12:30:59 +0100

    lab2 AI 2020

--- solvers/two_opt_with_candidate.py
+++ solvers/two_opt_with_candidate.py
@@ -5,14 +5,14 @@ import numpy as np
 class TwoOpt_CL:
 
     @staticmethod
-    def step2opt(solution, matrix_dist, distance, cand_list):
+    def step2opt(solution, matrix_dist, distance, cand_list, N):
         seq_length = len(solution)
         tsp_sequence = np.array(solution)
         uncrosses = 0
         for i in range(1, seq_length):
             for j in cand_list[i]:
-                new_tsp_sequence = TwoOpt_CL.swap2opt(tsp_sequence, i - 1, j)
-                new_distance = distance + TwoOpt_CL.gain(i - 1, j, tsp_sequence, matrix_dist)
+                new_tsp_sequence = TwoOpt_CL.swap2opt(tsp_sequence, i - 1, j, N)
+                new_distance = distance + TwoOpt_CL.gain(i - 1, j, tsp_sequence, matrix_dist, N)
                 if new_distance < distance:
                     uncrosses += 1
                     tsp_sequence = np.copy(new_tsp_sequence)
@@ -21,26 +21,27 @@ class TwoOpt_CL:
         return tsp_sequence, distance, uncrosses
 
     @staticmethod
-    def swap2opt(tsp_sequence, i, j):
+    def swap2opt(tsp_sequence, i, j, N):
         new_tsp_sequence = np.copy(tsp_sequence)
-        final_index = j + 1
+        final_index = j + 1 - N
         new_tsp_sequence[i:final_index] = np.flip(tsp_sequence[i:final_index], axis=0)
         return new_tsp_sequence
 
     @staticmethod
-    def gain(i, j, tsp_sequence, matrix_dist):
+    def gain(i, j, tsp_sequence, matrix_dist, N):
         old_link_len = (matrix_dist[tsp_sequence[i], tsp_sequence[i - 1]] + matrix_dist[
-            tsp_sequence[j], tsp_sequence[j + 1]])
+            tsp_sequence[j], tsp_sequence[j + 1 - N]])
         changed_links_len = (matrix_dist[tsp_sequence[j], tsp_sequence[i - 1]] + matrix_dist[
-            tsp_sequence[i], tsp_sequence[j + 1]])
+            tsp_sequence[i], tsp_sequence[j + 1 - N]])
         return - old_link_len + changed_links_len
 
     @staticmethod
     def local_search(solution, actual_len,  matrix_dist, CL):
         new_tsp_sequence = np.copy(np.array(solution))
+        N = len(solution)
         uncross = 0
         while True:
-            new_tsp_sequence, new_reward, uncr_ = TwoOpt_CL.step2opt(new_tsp_sequence, matrix_dist, actual_len, CL)
+            new_tsp_sequence, new_reward, uncr_ = TwoOpt_CL.step2opt(new_tsp_sequence, matrix_dist, actual_len, CL, N)
             uncross += uncr_
             if new_reward < actual_len:
                 actual_len = new_reward
