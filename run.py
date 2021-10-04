f2525c40f7a9adbe9b78c2ca281046f9d914b180
UmbertoJr <u.mele.coding@gmail.com>
2021-10-04 07:18:30 +0200

    update

--- run.py
+++ run.py
@@ -2,12 +2,14 @@ from IO_manager.io_tsp import TSP_Instance_Creator
 from IO_manager.io_kp import KP_Instance_Creator, distributions
 from IO_manager.generate_new_instances import generate_instance
 
+
 def run_trial_TSP():
     ic = TSP_Instance_Creator("standard", name_problem="eil76.tsp")
     ic.print_info()
     ic.plot_data()
     ic.plot_solution()
     print(ic.dist_matrix)
+
 
 def run_trial_KP():
     # print("random")
@@ -24,7 +26,6 @@ def run_trial_KP():
     # ic = KP_Instance_Creator(distributions[i])
     # ic.plot_data_scatter()
     # ic.plot_data_distribution()
-
 
 
 if __name__ == '__main__':
